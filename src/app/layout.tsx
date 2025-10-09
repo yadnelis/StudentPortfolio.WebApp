d9da1f6fe3c846e9c4a195fdbaf91074880ade85
yadnelis <yadnelis.g@gmail.com>
2025-10-09 13:09:51 -0400

    organize imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { Header } from "@/components/Header";
 import { MantineProvider } from "@mantine/core";
+
+import "@/app/globals.css";
+import "@mantine/core/styles.css";
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
