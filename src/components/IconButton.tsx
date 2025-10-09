bd22e4f2ecfab1dc1e22e029c2bb5184e460471b
yadnelis <yadnelis.g@gmail.com>
2025-10-09 13:13:51 -0400

    add IconButton

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,3 +1,4 @@
+import { Button, MantineSize } from "@mantine/core";
 import React, {
   Children,
   cloneElement,
@@ -7,21 +8,24 @@ import React, {
 } from "react";
 
 export interface IconButtonProps extends ComponentProps<"button"> {
-  size?: "sm" | "md" | "lg";
+  size?: MantineSize | (string & {});
   children: ReactElement<ComponentProps<"svg">>;
 }
 
 export const IconButton: FC<IconButtonProps> = ({
   children,
   size = "sm",
+  className,
   ...props
 }) => {
-  const twSize = size === "sm" ? "size-4" : size === "lg" ? "size-8" : "size-6";
+  const twSize = size === "sm" ? "size-4" : size === "lg" ? "size-6" : "size-5";
   return (
-    <button className={`appearance-none ${size}`} {...props}>
+    <button className={`appearance-none p-1 rounded-xs group/iconbutton inline-flex justify-center items-center ${className}`} {...props}>
       {Children.map(children, (child) =>
         cloneElement(child, {
-          className: twSize,
+          className: [twSize, React.Children.only(child).props.className,
+            "text-stone-600 group-hover/iconbutton:text-gray-400 group-active/iconbutton::text-gray-950 transition transition-50"
+           ].join(" "),
         })
       )}
     </button>
