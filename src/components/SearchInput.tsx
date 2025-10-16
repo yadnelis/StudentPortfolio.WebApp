39595bad88f95a1b42182350a737625477806b81
yadnelis <yadnelis.g@gmail.com>
2025-10-16 14:17:42 -0400

    search-input styling

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,31 +1,33 @@
 "use client";
 import { Hamburger, HamburgerIcon, Menu, Search, X } from "lucide-react";
 import { TextInput, TextInputProps } from "@/components/TextInput";
-import { useDebouncedState, useDebouncedValue } from '@mantine/hooks';
+import { useDebouncedState, useDebouncedValue } from "@mantine/hooks";
 import { FC, useEffect, useState } from "react";
 
 export interface SearchInputProps extends TextInputProps {
-  onChangeDebounceValue: (value: string) => void
+  onChangeDebounceValue: (value: string) => void;
 }
 
-export const SearchInput:FC<SearchInputProps> = ({onChangeDebounceValue, ...props}) => {
+export const SearchInput: FC<SearchInputProps> = ({
+  onChangeDebounceValue,
+  ...props
+}) => {
   const [value, setValue] = useState("");
   const [debounced] = useDebouncedValue(value, 300);
 
-  useEffect(()=> {
+  useEffect(() => {
     onChangeDebounceValue?.(debounced);
   }, [debounced]);
 
-
   return (
     <TextInput
-      leftButton={{ icon: <Menu/> }}
-      rightButton={{ icon: <X/>, onClick: (e) => setValue("") }}
+      leftButton={{ icon: <Menu /> }}
+      rightButton={{ icon: <X />, onClick: (e) => setValue("") }}
       size="md"
       placeholder="Search"
       value={value}
       onChange={(e) => setValue(e.target.value)}
-      className="!bg-slate-100 hover:!bg-white rounded-lg !border-0 !px-12"
+      className="!bg-slate-100 hover:!bg-white !rounded-lg !border-0 !px-12"
       {...props}
     />
   );
