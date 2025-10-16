c60a5869698088e4c074655129a0f5b441108830
yadnelis <yadnelis.g@gmail.com>
2025-10-16 14:17:33 -0400

    text-input styling

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -26,16 +26,34 @@ export const TextInput: FC<TextInputProps> = ({
   classNames,
   ...props
 }) => {
-  if(leftButton) {
-    props.leftSection = <IconButton size={"sm"} {...leftButton}>{leftButton.icon}</IconButton>
+  if (leftButton) {
+    props.leftSection = (
+      <IconButton size={"sm"} {...leftButton}>
+        {leftButton.icon}
+      </IconButton>
+    );
   }
-  if(rightButton) {
-    props.rightSection = <IconButton size={"sm"} {...rightButton}>{rightButton.icon}</IconButton>
+  if (rightButton) {
+    props.rightSection = (
+      <IconButton size={"sm"} {...rightButton}>
+        {rightButton.icon}
+      </IconButton>
+    );
   }
 
   return (
     <div className="relative">
-      <MTextInput {...props} classNames={{input: "bg-slate-50 " + (className??""), ...classNames}}/>
+      <MTextInput
+        {...props}
+        classNames={{
+          input: `bg-white hover:bg-slate-50 p-2 border-gray-200 rounded-xs border-2 text-sm focus:ring-blue-200/50 focus:ring-2 focus:border-gray-300 outline-none ${
+            className ?? ""
+          }`,
+          section: `inline-flex absolute h-full [&[data-position=right]]:right-0 [&[data-position=left]]:left-0 px-2 ${classNames?.section}`,
+          wrapper: `relative ${classNames?.wrapper}`,
+          ...classNames,
+        }}
+      />
     </div>
   );
 };
