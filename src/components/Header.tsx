163181d1b71f368c2e9602c63887ddde027c0407
yadnelis <yadnelis.g@gmail.com>
2025-10-28 22:52:34 -0400

    add the addStudentButton to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
 import { SearchInput } from "./SearchInput";
+import { AddStudentButton } from "./AddStudentButton";
 
 interface HeaderProps {}
 export const Header: FC<HeaderProps> = ({}) => {
   return (
-    <header className=" bg-lime-800 flex justify-center items-center h-13">
+    <header className=" bg-lime-800 flex justify-center items-center h-13 relative">
+      <AddStudentButton className="absolute left-5 top-6" />
       <SearchInput />
     </header>
   );
